f03d4d1dac66cd051b4fa3c2947f83001f788931
VictorTaelin <victortaelin@gmail.com>
2025-11-18 08:10:38 -0300

    guarded-deref must be used explicitly
    it interferes with optimal evaluation and can lead to interaction blowup
    on church nat exponentiation

--- main.hs
+++ main.hs
@@ -722,7 +722,7 @@ instance Show Term where
   show (Dp0 k)       = int_to_name k ++ "₀"
   show (Dp1 k)       = int_to_name k ++ "₁"
   show (Ref k)       = "@" ++ int_to_name k
-  show (Nam k)       = "" ++ k
+  show (Nam k)       = k
   show (Dry f x)     = show_app f [x]
   show Era           = "&{}"
   show (Sup l a b)   = "&" ++ int_to_name l ++ "{" ++ show a ++ "," ++ show b ++ "}"
@@ -800,8 +800,8 @@ int_to_name n = reverse (go n)
 lexeme :: ReadP a -> ReadP a
 lexeme p = skipSpaces *> p
 
-parse_nam :: ReadP String
-parse_nam = lexeme $ do
+parse_name :: ReadP String
+parse_name = lexeme $ do
   head <- satisfy (`elem` alphabet_first)
   tail <- munch (`elem` alphabet)
   return (head : tail)
@@ -840,6 +840,7 @@ parse_term_base = lexeme $ choice
   , parse_bol
   , parse_ctr
   , parse_nil
+  , parse_nam_or_dry
   , parse_var
   ]
 
@@ -863,7 +864,7 @@ parse_lam_or_swi = do
 
 parse_lam_body :: ReadP Term
 parse_lam_body = do
-  k <- parse_nam
+  k <- parse_name
   lexeme (char '.')
   body <- parse_term
   return (Lam (name_to_int k) body)
@@ -881,9 +882,9 @@ parse_lam_brace = between (lexeme (char '{')) (lexeme (char '}')) $ choice
 parse_dup :: ReadP Term
 parse_dup = do
   lexeme (char '!')
-  k <- parse_nam
+  k <- parse_name
   lexeme (char '&')
-  l <- parse_nam
+  l <- parse_name
   lexeme (char '=')
   v <- parse_term
   optional (lexeme (char ';'))
@@ -893,7 +894,7 @@ parse_dup = do
 parse_sup :: ReadP Term
 parse_sup = do
   lexeme (char '&')
-  l <- parse_nam
+  l <- parse_name
   between (lexeme (char '{')) (lexeme (char '}')) $ do
     a <- parse_term
     optional (lexeme (char ','))
@@ -928,7 +929,7 @@ parse_nat = lexeme (char 'ℕ') >> return Nat
 parse_ref :: ReadP Term
 parse_ref = do
   lexeme (char '@')
-  k <- parse_nam
+  k <- parse_name
   return (Ref (name_to_int k))
 
 parse_add :: ReadP Term
@@ -949,7 +950,7 @@ parse_number = read <$> munch1 isDigit
 
 parse_var :: ReadP Term
 parse_var = do
-  k <- parse_nam
+  k <- parse_name
   let kid = name_to_int k
   choice
     [ string "₀" >> return (Dp0 kid)
@@ -976,14 +977,21 @@ parse_ctr = do
 
 parse_nil :: ReadP Term
 parse_nil = do
-  lexeme (char '[')
-  lexeme (char ']')
+  lexeme (string "[]")
   return Nil
+
+parse_nam_or_dry :: ReadP Term
+parse_nam_or_dry = do
+  lexeme (char '^')
+  choice
+    [ do lexeme (char '('); f <- parse_term; x <- parse_term; lexeme (char ')'); return (Dry f x)
+    , do k <- parse_name; return (Nam k)
+    ]
 
 parse_func :: ReadP (Name, Term)
 parse_func = do
   lexeme (char '@')
-  k <- parse_nam
+  k <- parse_name
   lexeme (char '=')
   f <- parse_term
   return (name_to_int k, f)
@@ -1139,8 +1147,8 @@ wnf_enter e s (Ref k) = do
     Just f  -> do
       inc_inters e
       g <- alloc e f
-      -- wnf_enter e s g -- ← with this line: 162 interactions
-      wnf_enter e s (Gua (Nam ("@" ++ int_to_name k)) g) -- ← with this line: 765 interactions
+      wnf_enter e s g -- ← with this line: 162 interactions
+      -- wnf_enter e s (Gua (Nam ("@" ++ int_to_name k)) g) -- ← with this line: 765 interactions
     Nothing -> error $ "UndefinedReference: " ++ int_to_name k
 
 wnf_enter e s (Gua f g) = do
@@ -2255,81 +2263,79 @@ book = unlines
   , "@K4  = λs. !S0&K=s; !S1&K=λx0.(S0₀ (S0₁ x0)); λx1.(S1₀ (S1₁ x1))"
   , "@C8  = λs. !S0&C=s; !S1&C=λx0.(S0₀ (S0₁ x0)); !S2&C=λx1.(S1₀ (S1₁ x1)); λx3.(S2₀ (S2₁ x3))"
   , "@K8  = λs. !S0&K=s; !S1&K=λx0.(S0₀ (S0₁ x0)); !S2&K=λx1.(S1₀ (S1₁ x1)); λx3.(S2₀ (S2₁ x3))"
-  , "@id  = λa.a"
-  , "@not = λ{0:1+0;1+:λp.0}"
-  , "@dbl = λ{0:0;1+:λp.1+1+(@dbl p)}"
-  , "@and = λ{0:λ{0:0;1+:λp.0};1+:λp.λ{0:0;1+:λp.1+0}}"
-  , "@add = λ{0:λb.b;1+:λa.λb.1+(@add a b)}"
-  , "@sum = λ{0:0;1+:λp.!P&S=p;1+(@add P₀ (@sum P₁))}"
-  , "@foo = &L{λx.x,λ{0:0;1+:λp.p}}"
-  , "@gen = !F&A=@gen &A{λx.!X&B=x;&B{X₀,1+X₁},λ{0:&C{0,1};1+:λp.!G&D=F₁;!P&D=p;&D{(G₀ P₀),!H&E=G₁;!Q&E=P₁;1+&E{(H₀ Q₀),1+(H₁ Q₁)}}}}"
+  , "@id  = ^id ~> λa.a"
+  , "@not = ^not ~> λ{0:1+0;1+:λp.0}"
+  , "@dbl = ^dbl ~> λ{0:0;1+:λp.1+1+(@dbl p)}"
+  , "@and = ^and ~> λ{0:λ{0:0;1+:λp.0};1+:λp.λ{0:0;1+:λp.1+0}}"
+  , "@add = ^add ~> λ{0:λb.b;1+:λa.λb.1+(@add a b)}"
+  , "@sum = ^sum ~> λ{0:0;1+:λp.!P&S=p;1+(@add P₀ (@sum P₁))}"
+  , "@foo = ^foo ~> &L{λx.x,λ{0:0;1+:λp.p}}"
+  , "@gen = ^gen ~> !F&A=@gen &A{λx.!X&B=x;&B{X₀,1+X₁},λ{0:&C{0,1};1+:λp.!G&D=F₁;!P&D=p;&D{(G₀ P₀),!H&E=G₁;!Q&E=P₁;1+&E{(H₀ Q₀),1+(H₁ Q₁)}}}}"
   ]
 
 tests :: [(String,String)]
 tests =
   [ ("0", "0")
-  -- , ("(@not 0)", "1")
-  -- , ("(@not 1+0)", "0")
-  -- , ("!F&L=@id;!G&L=F₀;λx.(G₁ x)", "λa.a")
-  -- , ("(@and 0 0)", "0")
-  -- , ("(@and &L{0,1+0} 1+0)", "&L{0,1}")
-  -- , ("(@and &L{1+0,0} 1+0)", "&L{1,0}")
-  -- , ("(@and 1+0 &L{0,1+0})", "&L{0,1}")
-  -- , ("(@and 1+0 &L{1+0,0})", "&L{1,0}")
-  -- , ("λx.(@and 0 x)", "λa.(@and 0 a)")
-  -- , ("λx.(@and x 0)", "λa.(@and a 0)")
-  -- , ("(@sum 1+1+1+0)", "6")
-  -- , ("λx.(@sum 1+1+1+x)", "λa.3+(@add a 2+(@add a 1+(@add a (@sum a))))")
-  -- , ("(@foo 0)", "&L{0,0}")
-  -- , ("(@foo 1+1+1+0)", "&L{3,2}")
-  -- , ("λx.(@dbl 1+1+x)", "λa.4+(@dbl a)")
-  -- , ("("++f 2++" λX.(X λT0.λF0.F0 λT1.λF1.T1) λT2.λF2.T2)", "λa.λb.a")
-  -- , ("1+&L{0,1}", "&L{1,2}")
-  -- , ("1+&A{&B{0,1},&C{2,3}}", "&A{&B{1,2},&C{3,4}}")
-  -- , ("λa.!A&L=a;&L{A₀,A₁}", "&L{λa.a,λa.a}")
-  -- , ("λa.λb.!A&L=a;!B&L=b;&L{λx.(x A₀ B₀),λx.(x A₁ B₁)}", "&L{λa.λb.λc.(c a b),λa.λb.λc.(c a b)}")
-  -- , ("λt.(t &A{1,2} 3)", "&A{λa.(a 1 3),λa.(a 2 3)}")
-  -- , ("λt.(t 1 &B{3,4})", "&B{λa.(a 1 3),λa.(a 1 4)}")
-  -- , ("λt.(t &A{1,2} &A{3,4})", "&A{λa.(a 1 3),λa.(a 2 4)}")
-  -- , ("λt.(t &A{1,2} &B{3,4})", "&A{&B{λa.(a 1 3),λa.(a 1 4)},&B{λa.(a 2 3),λa.(a 2 4)}}")
-  -- , ("@gen", "&A{&B{λa.a,λa.1+a},&C{&D{λ{0:0;1+:λa.(@gen a)},&E{λ{0:0;1+:λa.1+(@gen a)},λ{0:0;1+:λa.2+(@gen a)}}},&D{λ{0:1;1+:λa.(@gen a)},&E{λ{0:1;1+:λa.1+(@gen a)},λ{0:1;1+:λa.2+(@gen a)}}}}}")
-  -- , ("λx.(@gen 2+x)", "&A{&B{λa.2+a,λa.3+a},&D{λa.(@gen a),&E{λa.2+(@gen a),λa.4+(@gen a)}}}")
-  -- , ("(@gen 2)", "&A{&B{2,3},&D{&C{0,1},&E{&C{2,3},&C{4,5}}}}")
-  -- , ("2 == 2", "#T")
-  -- , ("3 == 2", "#F")
-  -- , ("(λa.λb.a) == (λx.λy.x)", "#T")
-  -- , ("(λa.λb.a) == (λx.λy.y)", "#F")
-  -- , ("(λx.2+x) == (λy.2+y)", "#T")
-  -- , ("(λx.3+x) == (λy.2+y)", "#F")
-  -- , ("#F && #F", "#F")
-  -- , ("#F && #T", "#F")
-  -- , ("#T && #F", "#F")
-  -- , ("#T && #T", "#T")
-  -- , ("(λt.(t (λa.2+a) (λb.2+b))) == (λu.(u (λx.2+x) (λy.2+y)))", "#T")
-  -- , ("(λt.(t (λa.2+a) (λb.2+b))) == (λu.(u (λx.2+x) (λy.3+y)))", "#F")
-  -- , ("(@NOT @T)", "λa.λb.b")
-  -- , ("(@NOT (@NOT @T))", "λa.λb.a")
-  -- , ("(@C2 @NOT @T)", "λa.λb.a")
-  -- , ("@C2", "λa.λb.(a (a b))")
-  -- , ("(@ADD @C2 @C1)", "λa.λb.(a (a (a b)))")
-  -- , ("(@ADD @C1 λf.λx.(f x) @NOT)", "λa.λb.λc.(a b c)")
-  -- , ("(@ADD @C1 @C1 @NOT)", "λa.λb.λc.(a b c)")
-  -- , ("(@ADD @C2 @C2)", "λa.λb.(a (a (a (a b))))")
-  -- , ("(@ADD @C4 @C1)", "λa.λb.(a (a (a (a (a b)))))")
-  -- , ("(@ADD @C1 @C4)", "λa.λb.(a (a (a (a (a b)))))")
-  -- , ("(@ADD @C4 @C4)", "λa.λb.(a (a (a (a (a (a (a (a b))))))))")
-  -- , ("(@ADD @C1 @C4 @NOT @T)", "λa.λb.b")
-  -- , ("(@ADD @C4 @C1 @NOT @T)", "λa.λb.b")
-  -- , ("(@ADD @C2 @C4 @NOT @T)", "λa.λb.a")
-  -- , ("(@ADD @C4 @C2 @NOT @T)", "λa.λb.a")
-  -- , ("(@MUL @C4 @C2)", "λa.λb.(a (a (a (a (a (a (a (a b))))))))")
-  -- , ("(@MUL @C4 @C4)", "λa.λb.(a (a (a (a (a (a (a (a (a (a (a (a (a (a (a (a b))))))))))))))))")
-  -- , ("(@MUL @C4 @C2 @NOT @T)", "λa.λb.a")
-  -- , ("(@MUL @C4 @C4 @NOT @T)", "λa.λb.a")
-  -- , ("(@EXP @C4 @K2)", "λa.λb.(a (a (a (a (a (a (a (a (a (a (a (a (a (a (a (a b))))))))))))))))")
-  -- , ("(@C8 @K8 λB.λT.λF.(B F T) λt.λf.t)", "λa.λb.a")
-  , ("(@C4 @K8 λB.λT.λF.(B F T) λt.λf.t)", "λa.λb.a")
-  -- , ("(@C8)", "λa.λb.a")
+  , ("(@not 0)", "1")
+  , ("(@not 1+0)", "0")
+  , ("!F&L=@id;!G&L=F₀;λx.(G₁ x)", "λa.a")
+  , ("(@and 0 0)", "0")
+  , ("(@and &L{0,1+0} 1+0)", "&L{0,1}")
+  , ("(@and &L{1+0,0} 1+0)", "&L{1,0}")
+  , ("(@and 1+0 &L{0,1+0})", "&L{0,1}")
+  , ("(@and 1+0 &L{1+0,0})", "&L{1,0}")
+  , ("λx.(@and 0 x)", "λa.(and 0 a)")
+  , ("λx.(@and x 0)", "λa.(and a 0)")
+  , ("(@sum 1+1+1+0)", "6")
+  , ("λx.(@sum 1+1+1+x)", "λa.3+(add a 2+(add a 1+(add a (sum a))))")
+  , ("(@foo 0)", "&L{0,0}")
+  , ("(@foo 1+1+1+0)", "&L{3,2}")
+  , ("λx.(@dbl 1+1+x)", "λa.4+(dbl a)")
+  , ("("++f 2++" λX.(X λT0.λF0.F0 λT1.λF1.T1) λT2.λF2.T2)", "λa.λb.a")
+  , ("1+&L{0,1}", "&L{1,2}")
+  , ("1+&A{&B{0,1},&C{2,3}}", "&A{&B{1,2},&C{3,4}}")
+  , ("λa.!A&L=a;&L{A₀,A₁}", "&L{λa.a,λa.a}")
+  , ("λa.λb.!A&L=a;!B&L=b;&L{λx.(x A₀ B₀),λx.(x A₁ B₁)}", "&L{λa.λb.λc.(c a b),λa.λb.λc.(c a b)}")
+  , ("λt.(t &A{1,2} 3)", "&A{λa.(a 1 3),λa.(a 2 3)}")
+  , ("λt.(t 1 &B{3,4})", "&B{λa.(a 1 3),λa.(a 1 4)}")
+  , ("λt.(t &A{1,2} &A{3,4})", "&A{λa.(a 1 3),λa.(a 2 4)}")
+  , ("λt.(t &A{1,2} &B{3,4})", "&A{&B{λa.(a 1 3),λa.(a 1 4)},&B{λa.(a 2 3),λa.(a 2 4)}}")
+  , ("@gen", "&A{&B{λa.a,λa.1+a},&C{&D{λ{0:0;1+:λa.(gen a)},&E{λ{0:0;1+:λa.1+(gen a)},λ{0:0;1+:λa.2+(gen a)}}},&D{λ{0:1;1+:λa.(gen a)},&E{λ{0:1;1+:λa.1+(gen a)},λ{0:1;1+:λa.2+(gen a)}}}}}")
+  , ("λx.(@gen 2+x)", "&A{&B{λa.2+a,λa.3+a},&D{λa.(gen a),&E{λa.2+(gen a),λa.4+(gen a)}}}")
+  , ("(@gen 2)", "&A{&B{2,3},&D{&C{0,1},&E{&C{2,3},&C{4,5}}}}")
+  , ("2 == 2", "#T")
+  , ("3 == 2", "#F")
+  , ("(λa.λb.a) == (λx.λy.x)", "#T")
+  , ("(λa.λb.a) == (λx.λy.y)", "#F")
+  , ("(λx.2+x) == (λy.2+y)", "#T")
+  , ("(λx.3+x) == (λy.2+y)", "#F")
+  , ("#F && #F", "#F")
+  , ("#F && #T", "#F")
+  , ("#T && #F", "#F")
+  , ("#T && #T", "#T")
+  , ("(λt.(t (λa.2+a) (λb.2+b))) == (λu.(u (λx.2+x) (λy.2+y)))", "#T")
+  , ("(λt.(t (λa.2+a) (λb.2+b))) == (λu.(u (λx.2+x) (λy.3+y)))", "#F")
+  , ("(@NOT @T)", "λa.λb.b")
+  , ("(@NOT (@NOT @T))", "λa.λb.a")
+  , ("(@C2 @NOT @T)", "λa.λb.a")
+  , ("@C2", "λa.λb.(a (a b))")
+  , ("(@ADD @C2 @C1)", "λa.λb.(a (a (a b)))")
+  , ("(@ADD @C1 λf.λx.(f x) @NOT)", "λa.λb.λc.(a b c)")
+  , ("(@ADD @C1 @C1 @NOT)", "λa.λb.λc.(a b c)")
+  , ("(@ADD @C2 @C2)", "λa.λb.(a (a (a (a b))))")
+  , ("(@ADD @C4 @C1)", "λa.λb.(a (a (a (a (a b)))))")
+  , ("(@ADD @C1 @C4)", "λa.λb.(a (a (a (a (a b)))))")
+  , ("(@ADD @C4 @C4)", "λa.λb.(a (a (a (a (a (a (a (a b))))))))")
+  , ("(@ADD @C1 @C4 @NOT @T)", "λa.λb.b")
+  , ("(@ADD @C4 @C1 @NOT @T)", "λa.λb.b")
+  , ("(@ADD @C2 @C4 @NOT @T)", "λa.λb.a")
+  , ("(@ADD @C4 @C2 @NOT @T)", "λa.λb.a")
+  , ("(@MUL @C4 @C2)", "λa.λb.(a (a (a (a (a (a (a (a b))))))))")
+  , ("(@MUL @C4 @C4)", "λa.λb.(a (a (a (a (a (a (a (a (a (a (a (a (a (a (a (a b))))))))))))))))")
+  , ("(@MUL @C4 @C2 @NOT @T)", "λa.λb.a")
+  , ("(@MUL @C4 @C4 @NOT @T)", "λa.λb.a")
+  , ("(@EXP @C4 @K2)", "λa.λb.(a (a (a (a (a (a (a (a (a (a (a (a (a (a (a (a b))))))))))))))))")
+  , ("(@C8 @K8 λB.λT.λF.(B F T) λt.λf.t)", "λa.λb.a")
   ]
 
 run :: String -> String -> IO ()
@@ -2350,17 +2356,16 @@ run book_src term_src = do
 
 test :: IO ()
 test = forM_ tests $ \ (src, exp) -> do
-  env <- new_env $ read_book book
-  det <- collapse env $ read_term src
-  det <- show <$> snf env 1 det
+  !env <- new_env $ read_book book
+  !det <- collapse env $ read_term src
+  !det <- show <$> snf env 1 det
+  !itr <- readIORef (env_inters env)
   if det == exp then do
-    putStrLn $ "[PASS] " ++ src ++ " → " ++ det
+    putStrLn $ "[PASS] " ++ src ++ " → " ++ det ++ " | #" ++ show itr
   else do
     putStrLn $ "[FAIL] " ++ src
     putStrLn $ "  - expected: " ++ exp
     putStrLn $ "  - detected: " ++ det
-  !itr <- readIORef (env_inters env)
-  print itr
 
 main :: IO ()
 main = test
