61e53f1366e0103b1af2c21dca86678a7df76e6f
VictorTaelin <victortaelin@gmail.com>
2025-12-25 03:02:59 -0300

    wip: fast heap_take in single-thread

--- clang/heap/take.c
+++ clang/heap/take.c
@@ -1,4 +1,15 @@
 fn Term heap_take(u32 loc) {
+  if (__builtin_expect(THREAD_COUNT == 1, 1)) {
+    Term term = HEAP[loc];
+    if (__builtin_expect(term != 0, 1)) {
+      return term;
+    }
+    do {
+      cpu_relax();
+      term = HEAP[loc];
+    } while (term == 0);
+    return term;
+  }
   for (;;) {
     Term prev = __atomic_exchange_n(&HEAP[loc], 0, __ATOMIC_RELAXED);
     if (__builtin_expect(prev != 0, 1)) {
