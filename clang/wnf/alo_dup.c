efbdb64afcaa2a179348b05ccf63b4b478258316
Nicolas Abril <nicolas@higherorderco.com>
2026-02-18 16:35:23 +0100

    Remove redundant DUP allocation in wnf_alo_dup

--- clang/wnf/alo_dup.c
+++ clang/wnf/alo_dup.c
@@ -5,10 +5,10 @@
 // @{x',s} t
 fn Term wnf_alo_dup(u32 ls_loc, u32 len, Term book) {
   u32 book_loc = term_val(book);
-  u32 lab      = term_ext(book);
   u64 bind_ent = heap_alloc(2);
   Term alo_v = term_new_alo(ls_loc, len, book_loc + 0);
   heap_set(bind_ent + 0, alo_v);
   heap_set(bind_ent + 1, term_new_num(ls_loc));
-  return term_new_dup(lab, alo_v, term_new_alo((u32)bind_ent, len + 1, book_loc + 1));
+  Term alo_t = term_new_alo((u32)bind_ent, len + 1, book_loc + 1);
+  return alo_t;
 }
