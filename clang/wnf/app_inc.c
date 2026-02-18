52df7746c9ff938d6c99a3ddeb32e5be8c6469c9
Nicolas Abril <nicolas@higherorderco.com>
2026-02-18 17:02:31 +0100

    More memory reuse in wnf interactions

--- clang/wnf/app_inc.c
+++ clang/wnf/app_inc.c
@@ -7,13 +7,8 @@ fn Term wnf_app_inc(Term app, Term inc) {
   u32  inc_loc = term_val(inc);
   Term f       = heap_read(inc_loc);
   Term x       = heap_read(app_loc + 1);
-  // Reuse inc_loc for the new app, build new inc at app_loc
-  heap_set(inc_loc + 0, f);
-  // Need to allocate since app has 2 slots, inc has 1
-  u64 new_app_loc = heap_alloc(2);
-  heap_set(new_app_loc + 0, f);
-  heap_set(new_app_loc + 1, x);
-  Term new_app = term_new(0, APP, 0, new_app_loc);
-  heap_set(app_loc, new_app);
-  return term_new(0, INC, 0, app_loc);
+  // Build APP(f, x) in-place at app_loc, then store it under INC at inc_loc.
+  heap_set(app_loc + 0, f);
+  heap_set(inc_loc + 0, term_new(0, APP, 0, app_loc));
+  return inc;
 }
--- clang/wnf/ddu_num.c
+++ clang/wnf/ddu_num.c
@@ -1,16 +1,15 @@
 // ! X &(#n) = v; b
 // ---------------- DDU-NUM
 // ! X &n = v
-// b
+// b(X₀, X₁)
 fn Term wnf_ddu_num(Term lab_num, Term val, Term bod) {
   ITRS_INC("DDU-NUM");
   u32 lab      = term_val(lab_num);
-  u64 dup_loc  = heap_alloc(2);
+  u64 dup_loc  = heap_alloc(1);
   Term dp0     = term_new_dp0(lab, dup_loc);
   Term dp1     = term_new_dp1(lab, dup_loc);
   Term app0    = term_new_app(bod, dp0);
   Term app1    = term_new_app(app0, dp1);
-  heap_set(dup_loc + 0, val);
-  heap_set(dup_loc + 1, app1);
-  return term_new(0, DUP, lab, dup_loc);
+  heap_set(dup_loc, val);
+  return app1;
 }
